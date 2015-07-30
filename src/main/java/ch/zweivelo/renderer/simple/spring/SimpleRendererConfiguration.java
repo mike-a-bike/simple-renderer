3f3949c51178490c5eec89ec183c22fe36d4cef0
mike-a-bike <m.bieri@gmx.net>
2015-07-30 09:58:43 +0200

    spring configuration added

---
+++ src/main/java/ch/zweivelo/renderer/simple/spring/SimpleRendererConfiguration.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2015 Michael Bieri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.zweivelo.renderer.simple.spring;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring boot configuration for the simple renderer project.
+ * 
+ * @author <a href="mailto:m.bieri@gmx.net">Michael Bieri</a>
+ * @since 30.07.2015
+ * @version 0.1
+ */
+@Configuration
+public class SimpleRendererConfiguration {
+}
